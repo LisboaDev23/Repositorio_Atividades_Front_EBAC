67b3b725eb1e9487e11cf41b1b0be9239b21446e
Gabriel Pereira Lisboa <gabriel.pereira.lisboa@gmail.com>
2024-01-02 12:46:59 -0300

    commit mod 10

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#input-cpf').mask('000.000.000-00',{reverse:true})
+    $('#input-cep').mask('00000-000',{reverse:true})
+    $('#input-tel').mask('(00)00000-0000')
+})
