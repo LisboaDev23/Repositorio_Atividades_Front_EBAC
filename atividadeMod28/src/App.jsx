1b0aed4a35aa19cc54d080ee3de9aa50eeb4dbd0
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2024-05-16 23:00:31 -0300

    Implementada a estrutura do projeto, porém com erro na implementação do resultado do IMC, falta tbm colocar a lógica do click do botão para abrir a calculadora

---
+++ atividadeMod28/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Components/Main/Main'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
