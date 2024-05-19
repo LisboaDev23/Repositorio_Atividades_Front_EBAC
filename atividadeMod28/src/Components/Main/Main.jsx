1faf8d1b413f5e9d7004994e43a8a6f94099d69a
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2024-05-19 11:44:16 -0300

    inserção da lógica correta da apresentação do resultado e interatividade da primeira tela com o usuário

--- atividadeMod28/src/Components/Main/Main.jsx
+++ atividadeMod28/src/Components/Main/Main.jsx
@@ -2,71 +2,92 @@ import { useState } from 'react'
 import styles from './Main.module.css'
 
 function Header ({}){
-    
+    const [nomeUsuario, setNomeUsuario] = useState(null);
     const [botaoAbrirCalc, setBotaoAbrirCalc] = useState(false);
-    const [botaoResultado, setBotaoResultado] = useState(false);
-    const [valorAltura, setValorAltura] = useState(0);
-    const [valorPeso, setValorPeso] = useState(0);
-    const [valorImc, setValorImc] = useState(0);
+    const [telaInicialAberta, setTelaInicialAberta] = useState(true);
+    const [valorAltura, setValorAltura] = useState('');
+    const [valorPeso, setValorPeso] = useState('');
+    const [valorImc, setValorImc] = useState(null);
+    const [classificacaoTabela, setClassificacaoTabela] = useState('')
 
     function clickOpenCalc(){
-        setBotaoAbrirCalc(true);
-    }
-    function calculaImc(){
-        setValorImc(valorPeso/(valorAltura^2));
-    }
-    function determinaGrupoImc(){
-        if(valorImc<16.9){
-            return <b>Muito Abaixo do Peso</b>
-        }
-        if(valorImc>=18.5||valorImc<=18.4){
-            return <b>Abaixo do Peso</b>
-        }
-        if(valorImc>=18.5||valorImc<=24.9){
-            return <b>Peso Normal</b>
-        }
-        if(valorImc>=25||valorImc<=29.9){
-            return <b>Acima do Peso</b>
-        }
-        if(valorImc>=30||valorImc<=34.9){
-            return <b>Obesidade grau I</b>
-        }
-        if(valorImc>=35||valorImc<=40){
-            return <b>Obesidade grau II</b>
-        }
-        if(valorImc>40){
-            return <b>Obesidade grau III</b>
+        if(nomeUsuario != null){
+            setBotaoAbrirCalc(true);
+            setTelaInicialAberta(false);
+        } else {
+            alert("Insira seu nome!")
         }
     }
-    function clickBtnResultado(){
-        setBotaoResultado(true);
+
+    function calcularImc(peso, altura){
+        if(peso>0 && altura>0){
+            return (peso/(altura*altura)).toFixed(2);
+        } else{
+            alert("Insira os dados de forma correta!")
+        }
     }
+
+    const mudarValorImc = () =>{
+            const imcCalculado = calcularImc(parseFloat(valorPeso), parseFloat(valorAltura)); 
+            setValorImc(imcCalculado);
+            if(imcCalculado<16.9){
+                setClassificacaoTabela(<b>Muito Abaixo do Peso</b>)
+            }
+            else if(imcCalculado>=18.5 && imcCalculado<=18.4){
+                setClassificacaoTabela(<b>Abaixo do Peso</b>)
+            }
+            else if(imcCalculado>=18.5 && imcCalculado<=24.9){
+                setClassificacaoTabela(<b>Peso Normal</b>)
+            }
+            else if(imcCalculado>=25 && imcCalculado<=29.9){
+                setClassificacaoTabela(<b>Acima do Peso</b>)
+            }
+            else if(imcCalculado>=30 && imcCalculado<=34.9){
+                setClassificacaoTabela(<b>Obesidade grau I</b>)
+            }
+            else if(imcCalculado>=35 && imcCalculado<=40){
+                setClassificacaoTabela(<b>Obesidade grau II</b>)
+            }
+            else if(imcCalculado>40){
+                setClassificacaoTabela(<b>Obesidade grau III</b>)
+            }
+        }
+
+    function chamaResultado(){
+        mudarValorImc();
+        determinaGrupoImc();
+    }
+
     return (
         <>
+        {telaInicialAberta && (
             <div className={styles.divContainerHeader}>
                 <h3>Insira seu nome e aperte o botão para abrir a calculadora de IMC</h3>
-                <input className={styles.inputHeader} type="text" placeholder="Nome Completo"/>
+                <input className={styles.inputHeader} type="text" placeholder="Nome Completo" required onChange={ (e) => {setNomeUsuario(e.target.value)} }/>
                 <button onClick={clickOpenCalc}>ABRIR CALCULADORA</button>
             </div>
+        )}
 
+            {botaoAbrirCalc && (
             <div className={styles.containerCalculo}>
-                <h1>Olá Gabriel Lisboa</h1>
+                <h1>Olá {nomeUsuario}</h1>
                 <h3>Insira os dados abaixo para calcular seu IMC</h3>
                 <div  style={{marginTop:30}}>
-                    <input className={styles.inputDados} type="number" placeholder='ALTURA' onChange={ (e) => {setValorAltura(parseFloat(e.target.value))} }/>
+                    <input className={styles.inputDados} type="number" placeholder='ALTURA(m)' required onChange={ (e) => {setValorAltura(parseFloat(e.target.value))} }/>
                 </div>
                 <div style={{marginTop:18}}>
-                    <input className={styles.inputDados} type="number" placeholder='PESO' onChange={ (e) => {setValorPeso(parseFloat(e.target.value))} }/>
+                    <input className={styles.inputDados} type="number" placeholder='PESO(kg)' required onChange={ (e) => {setValorPeso(parseFloat(e.target.value))} }/>
                 </div>
-                <button onClick={clickBtnResultado}>CALCULAR</button>
+                <button onClick={chamaResultado}>CALCULAR</button>
             </div>
+            )}
             
-            {botaoResultado ? 
+            {valorImc && (
                 <div className={styles.containerResultado}>
-                    <h3>O valor do seu IMC é {calculaImc} </h3>
-                    <h4>Você está classificado como {determinaGrupoImc} </h4>
+                    <h3>O valor do seu IMC é {valorImc} </h3>
+                    <h4>Você está classificado como {classificacaoTabela} </h4>
                 </div> 
-                : <p>Aguardando inserção dos dados</p> 
+            ) 
             }
         </>
     )
