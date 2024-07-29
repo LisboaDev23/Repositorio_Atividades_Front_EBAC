2186af0845e10f3165f71f6ba27fb0659e505c0c
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2024-07-29 10:05:39 -0300

    Alteração no método de remoção para lógica correta!

--- cypress/e2e/ebac-lista-contatos/remocao.cy.js
+++ cypress/e2e/ebac-lista-contatos/remocao.cy.js
@@ -6,7 +6,10 @@ describe('Todos os testes para remoção de um contato', () => {
     } )
 
     it('O contato deve ser removido corretamente', () => {
-        cy.get('.delete').click({multiple: true})
+        cy.get('.delete').each(($componente) => {
+            //em cada componente que tiver essa classe .delete, clique nela
+            cy.wrap($componente).click()
+        })
         cy.get('sc-beqWaB.eQdhbg.contato').should('have.length', 0)
         cy.get('.sc-jTrPJq.bgKOAx h2').should('contain', '0 contatos na agenda')
     })
